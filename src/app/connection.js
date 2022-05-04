3dfdc5938ce6fa34780e64a72b1e118ced8f63b8
Jessie Lin <b19950828@gmail.com>
2022-05-04 22:42:48 +0800

    removed firebase info from connection.js

--- src/app/connection.js
+++ src/app/connection.js
@@ -8,13 +8,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId
-
-  // apiKey: "AIzaSyBB-MXIbYPJCDlX-gax9BTCuqaVDEmmizU",
-  // authDomain: "to-do-list-c7652.firebaseapp.com",
-  // projectId: "to-do-list-c7652",
-  // storageBucket: "to-do-list-c7652.appspot.com",
-  // messagingSenderId: "911108450735",
-  // appId: "1:911108450735:web:078ad94931e38e7a33675a"
 };
 const connect = initializeApp(firebaseConfig);
 const db = getFirestore(connect);
